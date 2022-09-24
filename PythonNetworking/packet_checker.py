040a4ed1258e438f21f786ad93e86e3fff129af9
AlKulakov <dongohago@mail.ru>
2022-09-24 05:01:26 -0400

    Code fixing

---
+++ PythonNetworking/packet_checker.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import scapy.all as scapy
+from scapy.layers import http
+
+def sniff(interface):
+    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
+
+def process_sniffed_packet(packet):
+    print(packet)
+
+sniff("eth0")
