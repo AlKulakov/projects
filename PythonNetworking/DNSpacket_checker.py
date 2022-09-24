b4479119c5bfdf17e8cf3eaa088cfab09691be31
AlKulakov <dongohago@mail.ru>
2022-09-24 17:55:10 +0300

    refactoring

--- PythonNetworking/DNSpacket_checker.py
+++ PythonNetworking/DNSpacket_checker.py
@@ -1,9 +1,10 @@
 #!usr/bin/env python
-
+import scapy.all as scapy
 import netfilterqueue
 
 def process_packet(packet):
-    print(packet)
+    scapy_packet = scapy.IP(packet.get_payload())
+    print(scapy_packet.show())
     packet.accept()
 
 queue = netfilterqueue.NetfilterQueue()
