c120e9222da6c2402edfc1b793758189b8233d48
Aleksandr Kulakov <dongohago@mail.ru>
2022-10-14 10:24:11 +0300

    Started writing python notebook

---
+++ PythonNotebook/notebook.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+intValue = 43 #Числовой тип
+stringValue = "ksdfds" #Строки
+floatValue = 2.32 #Числа с плавающей точкой
+listValue = [23, 32, "kdf"] # Нумерованный список, массив
+dictionaryValue = {"key1":34, "key2":43}  #Словарь в виде ключа и значений
+tupleValue = (20, "dfdfs", 93) #Не нумерованный список
+setValue  = {"fdsf", "dfsdf"} #Множества
+booleanValue = True # Булевой тип данных
+
+newString = "bfsodifhsldfk" 
+print(newString[1:9:2]) # [Начальный элемент:Конечный элемент:Шаг]
+
+len(stringValue) # Длина строки, массива и т.д.
+
+
