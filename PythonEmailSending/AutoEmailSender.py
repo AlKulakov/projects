d8cfb565eafc84d72f9c4956bfbfdf21831c2242
Aleksandr Kulakov <dongohago@mail.ru>
2022-10-16 16:02:22 +0300

    Added theme addition

--- PythonEmailSending/AutoEmailSender.py
+++ PythonEmailSending/AutoEmailSender.py
@@ -6,10 +6,13 @@ import argparse
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--target", dest="target", help="[-] There is a mail address of your target")
+    parser.add_argument("-s", "--subject", dest="subject", help="[-] There is a subject of message")
     parser.add_argument("-m", "--message", dest="message", help="[-] There is a message you want to send")
     args=parser.parse_args()
     if not args.target:
         parser.error("[-] There is an error, type -h for help")
+    if not args.subject:
+        subject="Nameless message"
     if not args.message:
         parser.error("[-] There is an error, type -h for help")
     return args
@@ -22,6 +25,10 @@ def sendEmail(targetEmail, messageText):
 
 arg = get_args()
 targetEmail = arg.target
+subject = arg.subject
 text = arg.message
-message = f'Subject: Automatic mail sending\n\nFrom: Bagitaf Georgiy\nTo: {targetEmail}\n\n{text}'
+message = f'Subject: {subject}\n\nFrom: Bagitaf Georgiy\nTo: {targetEmail}\n\n{text}'
 sendEmail(targetEmail, message)
+
+
+
