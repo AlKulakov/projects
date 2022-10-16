936844cd99a80e951c92924c53231d54ab244b3e
AlKulakov <108340701+AlKulakov@users.noreply.github.com>
2022-10-16 22:58:11 +0300

    Added program starter

--- PythonImportantScripts/PythonBrowserOpenAutomation/WorkSetupAutomation.py
+++ PythonImportantScripts/PythonBrowserOpenAutomation/WorkSetupAutomation.py
@@ -1,10 +1,13 @@
 #!usr/bin/env python3
 
 import webbrowser as wb
+import os
 
 def workauto():
     IEpath = 'C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe %s'
-    URLS = ("github.com/AlKulakov",
+    ProgramPath = 'C:\\Program Files\\Notepad++\\notepad++.exe'
+    os.startfile(ProgramPath)
+    URLS = ("github.com",
             "google.com")
     for url in URLS:
         wb.get(IEpath).open(url)
